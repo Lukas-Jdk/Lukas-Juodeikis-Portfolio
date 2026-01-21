4fee8f8c52bc40a12c79ae266fbfca0e1bd226bb
Lukas-Jdk <jdklukac@gmail.com>
2026-01-21 10:17:02 +0100

    updated skills design

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -17,11 +17,11 @@ const data: { title: string; items: Skill[] }[] = [
   {
     title: "Frontend",
     items: [
-      { name: "HTML", value: 88, Icon: FaHtml5, color: "#E34F26" },
-      { name: "CSS", value: 84, Icon: FaCss3Alt, color: "#1572B6" },
-      { name: "JavaScript", value: 86, Icon: FaJs, color: "#F7DF1E" },
-      { name: "TypeScript", value: 80, Icon: SiTypescript, color: "#3178C6" },
-      { name: "React", value: 83, Icon: FaReact, color: "#61DAFB" },
+      { name: "HTML", value: 90, Icon: FaHtml5, color: "#E34F26" },
+      { name: "CSS", value: 90, Icon: FaCss3Alt, color: "#1572B6" },
+      { name: "JavaScript", value: 80, Icon: FaJs, color: "#F7DF1E" },
+      { name: "TypeScript", value: 78, Icon: SiTypescript, color: "#3178C6" },
+      { name: "React", value: 78, Icon: FaReact, color: "#61DAFB" },
       { name: "Next.js", value: 78, Icon: SiNextdotjs, color: "#FFFFFF" },
     ],
   },
@@ -43,7 +43,7 @@ const data: { title: string; items: Skill[] }[] = [
   {
     title: "APIs & Tools",
     items: [
-      { name: "REST APIs", value: 82, Icon: FiLink, color: "#00CCFF" },
+      { name: "REST APIs", value: 76, Icon: FiLink, color: "#00CCFF" },
       { name: "Git", value: 80, Icon: FaGitAlt, color: "#F05032" },
       
     ],
